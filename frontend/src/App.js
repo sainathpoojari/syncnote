0fef7cb7209e68a8c5e581c473605247f578cae4
sainath <sainathpoojari8@gmail.com>
2022-03-22 18:08:28 +0530

    publish folder

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import  Alert  from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import React,{useState} from 'react';
+
+
+function App() {
+    const [alert,setAlert] = useState(null);
+    const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} message="This is amazing React course" />
+          <div className='container'>
+            <Routes>
+              <Route exact path="/" element={<Home showAlert={showAlert} />} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+              <Route exact path="/signup" element={<Signup showAlert={showAlert} />} />
+            </Routes>
+          </div>
+          {/* <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+            </Switch>
+          </div> */}
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
